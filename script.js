9b9c9821c45aefee81048bf8530ef52062cf7e8c
adu <muhammadumar1220@gmail.com>
2021-10-29 22:59:14 +0100

    Add

---
+++ script.js
@@ -0,0 +1,8 @@
+// this file contains the code for rock paper scissors game 
+const gameItems = ['rock', 'paper', 'sccissors',];
+function computerPlay() {
+    const computerSelector = gameItems[Math.floor(Math.random() * gameItems.length)];
+    return computerSelector;
+
+}
+const 
